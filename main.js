363616a1276f2949d995354ee2964fe66ce2823f
Gurunath32 <73350586+Gurunath32@users.noreply.github.com>
2021-03-25 21:29:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+//teachable link-https://teachablemachine.withgoogle.com/models/ojGjL3n9M/
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+})
+
+camera=document.getElementById("camera");
+Webcam.attach(camera);
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log("ml5 Version:",ml5.version);
+
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ojGjL3n9M/modal.json',modelloaded);
+function modelloaded() {
+    console.log("Modal Loaded!")
+}
